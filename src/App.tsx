508c3bc17aa851f362dd1df9efcd162904150a36
aaaa1597 <tokushi2011@gmail.com>
2024-02-12 18:46:45 +0900

    完成。

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { useZxing } from "react-zxing";
 import ResultPoint from '@zxing/library/esm/core/ResultPoint';
 
-const FBXModel = (props:{setActionName: React.Dispatch<React.SetStateAction<string>>}) => {
+const FBXModel = (props:{setActionName: React.Dispatch<React.SetStateAction<string>>, detectcount: number}) => {
   /* FBXモデル読込み */
   const fbx = useLoader(FBXLoader, "assets/Ch09_nonPBR.fbx");
   /* AnimationClip(s)読込み */
@@ -21,7 +21,7 @@ const FBXModel = (props:{setActionName: React.Dispatch<React.SetStateAction<stri
   const animNames = ['BreakdanceEnding2', 'BreakdanceUprockVar1', 'HipHopDancing', 'NorthernSoulSpin', 'SwingDancing', 'BreakdanceEnding1']
   /* 変数定義 */
   const mixer = useRef<THREE.AnimationMixer>();
-  const [ animIdx, setAnimIdx ] = useState<number>(0);
+  const [ animIdx, setAnimIdx ] = useState<number>(-1);
   const animActions = useMemo(() => [] as THREE.AnimationAction[], [])
 
   /* 初期化 */
@@ -32,10 +32,16 @@ const FBXModel = (props:{setActionName: React.Dispatch<React.SetStateAction<stri
       if(!mixer.current) return;
       animActions[idx] = mixer.current.clipAction(val[0])
     })
-    new Promise(() => setTimeout(() => {0}, 1000)).then(()=>animActions[0].play())
   }, [])
 
-  /* モーション切替え処理 */
+  useEffect(() => {
+    if(props.detectcount > 0 && animIdx == -1) {
+      setAnimIdx(0);
+      animActions[0].play();
+    }
+  }, [props.detectcount])
+
+    /* モーション切替え処理 */
   useEffect(() => {
     const act: THREE.AnimationAction = animActions[animIdx]
     act?.reset().fadeIn(0.3).play()
@@ -47,13 +53,17 @@ const FBXModel = (props:{setActionName: React.Dispatch<React.SetStateAction<stri
 
   /* FPS処理 */
   useFrame((state, delta) => {
+    if(animIdx == -1) return;
+
     if(mixer.current)
       mixer.current.update(delta);
     const durationtime: number= animActions[animIdx].getClip().duration
     const currenttime: number = animActions[animIdx].time
     if(currenttime/durationtime > 0.9/*90%を超えたら次のモーションへ*/) {
-      const index: number = (animIdx+1) % (animCrips.length)
-      setAnimIdx( index )
+      if(animIdx+1 == animCrips.length)
+        setAnimIdx(-1)
+      else
+        setAnimIdx( (animIdx+1) % (animCrips.length) )
     }
   });
 
@@ -62,12 +72,8 @@ const FBXModel = (props:{setActionName: React.Dispatch<React.SetStateAction<stri
   )
 }
 
-const ZxingQRCodeReader = (props:{setSize: React.Dispatch<React.SetStateAction<React.CSSProperties>>}) => {
-  {/*  デバッグ用canvas */}
-  const [context, setContext] = useState<CanvasRenderingContext2D>();
-  const [points4, setPoints4] = useState<ResultPoint[]>();
-  const [points3, setPoints3] = useState<ResultPoint[]>();
-
+const ZxingQRCodeReader = (props:{setSize: React.Dispatch<React.SetStateAction<strSize>>, setCenter: React.Dispatch<React.SetStateAction<strPoint>>, setDetecte: React.Dispatch<React.SetStateAction<number>>}) => {
+  const detectcount = useRef<number>(0);
   const { ref } = useZxing({
     constraints: {
       audio: false,
@@ -85,10 +91,15 @@ const ZxingQRCodeReader = (props:{setSize: React.Dispatch<React.SetStateAction<R
 //        setResult(result.getText());
 
       const points: ResultPoint[] = result.getResultPoints()
-      if(points.length%4 == 0)
-        setPoints4(points);
-      else if(points.length%3 == 0)
-        setPoints3(points);
+
+      /* 中心を求める */
+      const center: ResultPoint = (points.length==4) ? CenterofLine(points[2], points[0]) :
+                                  (points.length==3) ? CrossLineLine( points[0], points[1], points[2], points[3]) :
+                                  new ResultPoint(0,0);
+      props.setCenter({x: center.getX(), y: center.getY()});
+      props.setDetecte(detectcount.current++);
+      console.log('aaa detected!! count=', detectcount);
+
       console.log(points.length, " -----[0]: ", points[0]?.getX(), " ,", points[0]?.getY(),)
       console.log(points.length, " -----[1]: ", points[1]?.getX(), " ,", points[1]?.getY(),)
       console.log(points.length, " -----[2]: ", points[2]?.getX(), " ,", points[2]?.getY(),)
@@ -100,98 +111,25 @@ const ZxingQRCodeReader = (props:{setSize: React.Dispatch<React.SetStateAction<R
   useEffect(() => {
     if(!ref.current) return;
     props.setSize({width: ref.current.videoWidth, height: ref.current.videoHeight});
-    const canvas = document.getElementById("canvas") as HTMLCanvasElement
-    canvas.width = ref.current.videoWidth;
-    canvas.width = ref.current.videoWidth;
-    const context = canvas.getContext("2d")
-    if(!context) return;
-    setContext(context);
   }, [ref.current?.videoWidth, ref.current?.videoHeight]);
 
   console.log("ref.current?.videoxxx=(", ref.current?.videoWidth, ",", ref.current?.videoHeight, ")" );
 
-  {/*  デバッグ用 3点取れた時の確認 */}
-  useEffect(() => {
-    if(!context) return;
-    if(!points3) return;
-    /* 対角線の中点を求める */
-    const xpwr = Math.abs(points3[2].getX() - points3[0].getX());
-    const xm   = Math.min(points3[2].getX() , points3[0].getX()) + xpwr/2;
-    const ypwr = Math.abs(points3[2].getY() - points3[0].getY());
-    const ym   = Math.min(points3[2].getY() , points3[0].getY()) + ypwr/2;
-    const m = new ResultPoint( xm, ym);
-    context.clearRect(0, 0, 1920, 1080)
-    /* 中点を描画 */
-    context.beginPath();
-    context.arc(m.getX(), m.getY(), 10, 0, 2*Math.PI, false);
-    context.fillStyle = 'green';
-    context.fill();
-    context.stroke()
-    /* 矩形 */
-    context.beginPath()
-    context.moveTo( points3[0].getX(), points3[0].getY())
-    context.lineTo( points3[1].getX(), points3[1].getY())
-    context.lineTo( points3[2].getX(), points3[2].getY())
-    /* 中心線 */
-    context.moveTo( points3[0].getX(), points3[0].getY())
-    context.lineTo( m.getX(), m.getY())
-    context.moveTo( points3[1].getX(), points3[1].getY())
-    context.lineTo( m.getX(), m.getY())
-    context.moveTo( points3[2].getX(), points3[2].getY())
-    context.lineTo( m.getX(), m.getY())
-    /* 描画 */
-    context.strokeStyle = "red";
-    context.lineWidth = 2;
-    context.stroke()
-    context.font = "48px serif";
-    context.fillText("0",points3[0].getX(), points3[0].getY())
-    context.fillText("1",points3[1].getX(), points3[1].getY())
-    context.fillText("2",points3[2].getX(), points3[2].getY())
-  }, [points3]);
-
-  {/*  デバッグ用 4点取れた時の確認 */}
-  useEffect(() => {
-    if(!context) return;
-    if(!points4) return;
-    context.clearRect(0, 0, 1920, 1080)
-    /* 中点を求める */
-    const m = CrossLineLine( points4[0], points4[1], points4[2], points4[3]);
-    /* 中点を描画 */
-    context.beginPath();
-    context.arc(m.getX(), m.getY(), 10, 0, 2*Math.PI, false);
-    context.fillStyle = 'green';
-    context.fill();
-    context.stroke()
-    /* 矩形 */
-    context.beginPath()
-    context.moveTo( points4[0].getX(), points4[0].getY())
-    context.lineTo( points4[1].getX(), points4[1].getY())
-    context.lineTo( points4[2].getX(), points4[2].getY())
-    context.lineTo( points4[3].getX(), points4[3].getY())
-    /* 対角線 */
-    context.moveTo( points4[0].getX(), points4[0].getY())
-    context.lineTo( points4[2].getX(), points4[2].getY())
-    context.moveTo( points4[1].getX(), points4[1].getY())
-    context.lineTo( points4[3].getX(), points4[3].getY())
-    context.strokeStyle = "red";
-    context.lineWidth = 2;
-    context.stroke()
-    context.font = "48px serif";
-    context.fillText("0",points4[0].getX(), points4[0].getY())
-    context.fillText("1",points4[1].getX(), points4[1].getY())
-    context.fillText("2",points4[2].getX(), points4[2].getY())
-    context.fillText("3",points4[3].getX(), points4[3].getY())
-  }, [points4]);
-
-
   return (
-    <>
     <video ref={ref} />
-      {/*  デバッグ用canvas */}
-      <canvas id="canvas" width="1920" height="1080" style={{ position: "absolute", left: "0px",  top: "0px", background: "#0088ff44"}}></canvas>
-    </>
   );
 };
+
+/**********************/
+/* 線分の中心点を求める */
+/**********************/
+const CenterofLine = (p00: ResultPoint, p01: ResultPoint) => {
+    const xpwr = Math.abs(p00.getX() - p01.getX());
+    const xm   = Math.min(p00.getX() , p01.getX()) + xpwr/2;
+    const ypwr = Math.abs(p00.getY() - p01.getY());
+    const ym   = Math.min(p00.getY() , p01.getY()) + ypwr/2;
+    return new ResultPoint( xm, ym);
+}
 
 /**********************/
 /* 2線分の交点を求める */
@@ -209,18 +147,23 @@ const CrossLineLine = (p00: ResultPoint, p01: ResultPoint, p02: ResultPoint, p03
   return new ResultPoint( x, y);
 }
 
+type strSize = { width: number; height: number; };
+type strPoint= { x:     number; y:      number; };
+
 const App = () => {
   const [actionName, setActionName] = useState<string>('aaabbb');
-  const [size, setSize] = useState<React.CSSProperties>({width: "300px", height: "200px"});
+  const [size, setSize] = useState<strSize>({width: 300, height: 200});
+  const [center, setCenter] = useState<strPoint>({x: 0, y: 0});
+  const [detectcount, setDetecte] = useState<number>(0);
 
   return (
     <div>
-      <ZxingQRCodeReader setSize={setSize}/>
-      <Canvas camera={{ position: [3, 1, 3] }} style={{ position: "absolute", left: "0px",  top: "0px", width: `${size.width}px`,  height: `${size.height}px`,}}>
+      <ZxingQRCodeReader setSize={setSize} setCenter={setCenter} setDetecte={setDetecte}/>
+      <Canvas camera={{ position: [3, 1, 3] }} style={{ position: "absolute", left: `${center.x-(size.width/2)}px`,  top: `${center.y-(size.height/2)}px`, width: `${size.width}px`,  height: `${size.height}px`,}}>
         <ambientLight intensity={2} />
         <pointLight position={[40, 40, 40]} />
         <Suspense fallback={null}>
-          <FBXModel setActionName={setActionName}/>
+          <FBXModel setActionName={setActionName} detectcount={detectcount}/>
         </Suspense>
         <OrbitControls />
         <axesHelper args={[5]} />
